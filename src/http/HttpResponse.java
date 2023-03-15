34637ede58ca43e1c405d36a46e2301920a981c4
omarmohamedkh <mohamed.omar67492@gmail.com>
2023-03-15 20:52:36 +0200

    Fix merge error

--- src/http/HttpResponse.java
+++ src/http/HttpResponse.java
@@ -8,16 +8,12 @@ import java.util.HashMap;
 
 public abstract class HttpResponse {
     public final HashMap<String, String> headers = new HashMap<>();
-    
+
     protected HttpStatusCode httpStatusCode;
     protected HttpContentType httpContentType;
     protected boolean isFile = false;
     protected boolean useGzip = false;
     protected ByteBuffer buffer = null;
-
-    public HttpResponse(HttpContentType httpContentType) {
-        this(HttpStatusCode.OK, httpContentType);
-    }
 
     public boolean isFileResponse() {
         return isFile;
